fab725217a6ec85d22c54a36a87f598674af7a15
Mehran Mahmoudi <mehr.moudi@gmail.com>
2018-08-02 11:17:57 -0600

    Prepare for Java code

--- Database/commit_url_function.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `commit_url`(commit_hash char(40), project_url varchar(1000)) RETURNS varchar(1000) CHARSET utf8
-BEGIN
-RETURN CONCAT("[" , substring(commit_hash, 1, 7) , "](", project_url, "/commit/", commit_hash, ")");
-END
